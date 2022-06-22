64574912137a2f9a2dc960b59c46a175d9abe925
kasturi28 <kasturipansare2818@gmail.com>
2022-06-22 23:42:54 +0530

    uc3_added_checking_snake_ladder_sameposition

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,18 +2,39 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
+	public static final int SAME_POSITION = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to snake and ladder game...");
-		
+
 		int position = 0;
-        System.out.println("Starting position of player = " + position);
-        
-        Random random = new Random();
-        int x1 = random.nextInt(6);
-        
-        System.out.println("Player got the position " +x1);
-        
+		System.out.println("Starting position of player = " + position);
+
+		Random random = new Random();
+		int dice = random.nextInt(6);
+		int check = random.nextInt(3);
+
+		System.out.println("Player got the position " + dice);
+
+		switch (check) {
+		case SAME_POSITION:
+			System.out.println("Player remains on the same position..." + dice);
+			break;
+		case LADDER:
+			position = position + dice;
+			System.out.println("Player get ladder & positon of the player = " + position);
+			break;
+		case SNAKE:
+			position = position - dice;
+			if (position < 0) {
+				position = 0;
+				System.out.println("Player get snake & positon of the player = " + position);
+			}
+
+		}
 
 	}
 
